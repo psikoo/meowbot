68f499b14be4399c7b90f57cfbec9751639631c6
psikoo <91342186+psikoo@users.noreply.github.com>
2026-09-19 23:15:39 +0200

    a

--- src/db/connection.js
+++ src/db/connection.js
@@ -6,7 +6,7 @@ const pool = new Pool({
   host: "db",
   database: process.env.POSTGRES_DB,
   password: process.env.POSTGRES_PASSWORD,
-  port: 5433,
+  port: 5432,
   max: 20,
   idleTimeoutMillis: 30000,
 });
